f246841d3a8021340586748c3a09684090257f04
Demian Florentin <demian@sinergetica.com>
2015-04-01 15:04:10 -0400

    parlamentary data extraction first iteration

--- scrap.py
+++ scrap.py
@@ -8,10 +8,10 @@ import httplib
 import json
 import urllib
 import unicodedata
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, CData
 from HTMLParser import HTMLParser
 
-import request_content
+import xml.etree.ElementTree as ET
 
 application_x_www_form_urlencoded='application/x-www-form-urlencoded'
 headers = {}
@@ -66,11 +66,6 @@ class SilpyScrapper(object):
             else:
                 key, val = header.split(":")
                 headers_dict[key.strip()] = val.strip()
-
-            print "##################################################"
-            print header
-            print "##################################################"
-
         return headers_dict
     
     @DeprecationWarning
@@ -95,49 +90,6 @@ class SilpyScrapper(object):
             if str_images in src:
                 ids.append(src[len(str_images)+1: len(src)].replace('.jpg',''))
         return ids
-
-    def _extract_parlamentary_data(self, html):
-
-        soup = BeautifulSoup(html)
-        tbody = soup.find(id="formMain:dataTable_data")
-        tr_list = tbody.find_all("tr")
-        rows = []
-        row = {}
-        str_images = '/images'
-        for tr in tr_list:
-            #this is a table, of course it has a fixed length
-            #0 = row_id, incremental
-            #1 = img (we extract the parlamentary id from the image name
-            #2 = name
-            #3 = committees: div with list of divs
-            #4 = projects: link using the extracted id from (2)
-        
-            td_list = tr.find_all("td")
-            row['index'] = td_list[0].text.strip()
-            row['name'] = td_list[2].text.strip()
-            row['projects'] = td_list[4].text.strip()
-            row_index = int(row['index'])-1
-            #extraction of parlamentary id
-            src = td_list[1].div.img['src']
-            if str_images in src:
-                row['id'] = src[len(str_images)+1: len(src)].replace('.jpg','')
-
-            #extraction of formMain
-            lis = td_list[3].div.div.find_all("li")
-            committees = []
-            for li in lis:
-                committee = {'text': li.text.strip(), 'formMain': li.a['id']}
-                formMain = committee['formMain']+ '=' + committee['formMain']
-                viewState = "-3644735490815418626%3A-2324997477277913544"
-                body = 'javax.faces.ViewState='+ viewState + '&' + formMain
-                print (committee['text'], body)
-                committees.append(committee)
-        
-            row['committees'] = committees
-    
-            print row['index'] + " - " + row['name'] +" - " + row['id']
-            print "##################################################################"
-
     def _download_attachment(self):
         #TODO:
         # content = 'formMain=formMain&formMain%3AdataTableDetalle%3A0%3Aj_idt178=&javax.faces.ViewState=-3644735490815418626%3A-2324997477277913544'
@@ -155,6 +107,57 @@ class SilpyScrapper(object):
         input = soup.find(id='javax.faces.ViewState')
         self.viewState =  input['value']
         
+    def _extract_parlamentary_data(self, html):
+        #print html
+        partial_soup = BeautifulSoup(html)
+        cdata_contents = []
+        for cd in partial_soup.findAll(text=True):
+            if isinstance(cd, CData):
+                cdata_contents.append(cd)
+        table_section = cdata_contents[0]
+        viewState = cdata_contents[1]
+        
+        soup = BeautifulSoup(table_section)
+        tbody = soup.find(id="formMain:dataTable_data")
+        tr_list = tbody.find_all("tr")
+        rows = []
+        row = {}
+        str_images = '/images'
+        for tr in tr_list:
+            #this is a table, of course it has a fixed length
+            #0 = row_id, incremental
+            #1 = img (we extract the parlamentary id from the image name
+            #2 = name
+            #3 = committees: div with list of divs
+            #4 = projects: link using the extracted id from (2)
+
+            td_list = tr.find_all("td")
+            row['index'] = td_list[0].text.strip()
+            row['name'] = td_list[2].text.strip()
+            row['projects'] = td_list[4].text.strip()
+            row_index = int(row['index'])-1
+            #extraction of parlamentary id
+            src = td_list[1].div.img['src']
+            if str_images in src:
+                row['id'] = src[len(str_images)+1: len(src)].replace('.jpg','')
+
+                #extraction of formMain
+                lis = td_list[3].div.div.find_all("li")
+                committees = []
+                for li in lis:
+                    committee = {'text': li.text.strip(), 'formMain': li.a['id']}
+                    formMain = committee['formMain']+ '=' + committee['formMain']
+                    #viewState = self.viewState # "-3644735490815418626%3A-2324997477277913544"
+                    body = 'javax.faces.ViewState='+ viewState + '&' + formMain
+                    print (committee['text'], body)
+                    committees.append(committee)
+
+                    row['committees'] = committees
+
+                    print row['index'] + " - " + row['name'] +" - " + row['id']
+                    print "##################################################################"
+
+
     def init_session_values(self):
         #TODO: validate reponse status (decorate?)
         response, data = self._execute_REST_call('GET', '/main.pmf', None, headers, application_x_www_form_urlencoded)
@@ -203,21 +206,17 @@ class SilpyScrapper(object):
         headers['Connection'] = "keep-alive"
         headers['Pragma'] = "no-cache"
         headers['Cache-Control'] = "no-cache"
-        
+
+        #THIS WORKS
         data = "javax.faces.partial.ajax=true&javax.faces.source=formMain:cmdBuscarParlamentario"\
                +"&javax.faces.partial.execute=@all"\
                +"&javax.faces.partial.render=formMain&formMain:cmdBuscarParlamentario=formMain:cmdBuscarParlamentario"\
                +"&formMain=formMain&formMain:idOrigen_input=" + origin + "&formMain:idPeriodoLegislativo_input=" + period \
                +"&javax.faces.ViewState=" + self.viewState 
 
-
-        #data = "javax.faces.partial.ajax%3Dtrue%26javax.faces.source%3DformMain%3AcmdBuscarParlamentario%26javax.faces.partial.execute%3D%40all%26javax.faces.partial.render%3DformMain%26formMain%3AcmdBuscarParlamentario%3DformMain%3AcmdBuscarParlamentario%26formMain%3DformMain%26formMain%3AidOrigen_input%3D"  + origin + "%26formMain%3AidPeriodoLegislativo_input%3D"+ period + "%26javax.faces.ViewState%3D" + self.viewState
+        # data = "javax.faces.partial.ajax%3Dtrue%26javax.faces.source%3DformMain%3AcmdBuscarParlamentario%26javax.faces.partial.execute%3D%40all%26javax.faces.partial.render%3DformMain%26formMain%3AcmdBuscarParlamentario%3DformMain%3AcmdBuscarParlamentario%26formMain%3DformMain%26formMain%3AidOrigen_input%3D"  + origin + "%26formMain%3AidPeriodoLegislativo_input%3D"+ period + "%26javax.faces.ViewState%3D" + self.viewState
         
         _url = '/formulario/ListarParlamentario.pmf'
-
-        print "##################################################"
-        print self.headers
-        print "##################################################"
         return self._execute_REST_call('POST', _url, data, headers, application_x_www_form_urlencoded)
     
     def get_presented_projects(self, parlamentary_id):
@@ -233,17 +232,31 @@ class SilpyScrapper(object):
     def _extract_appointments(self):
         pass
 
-
         
-scraper = SilpyScrapper()
-response, data = scraper.init_session_values()
-response, data = scraper.get_parlamentary_list('S')
-print data
+scrapper = SilpyScrapper()
+response, data = scrapper.init_session_values()
+response, data = scrapper.get_parlamentary_list('D')
+scrapper._extract_parlamentary_data(data)
+#print data
 
-# html = ''
-# listarParlamentario = open('resources/buscar_parlamentarios_update.html')
-# for l in listarParlamentario:
-#     html +=l 
+
+update_data = 'resources/buscar_parlamentarios_update.html'
+lista_parlamentarios = 'resources/lista_parlamentarios.html'
+
+html = ''
+htmlfile = open(lista_parlamentarios)
+for l in htmlfile:
+    html +=l 
+#print html
+
+#scrapper._extract_parlamentary_data(html)
+
+
+#print table_section.find("formMain:dataTable_data")
+#soup2 = BeautifulSoup(table_section)
+#tbody = soup2.find(id="formMain:dataTable_data")
+
+#_extract_parlamentary_data(table_section, "-3644735490815418626%3A-2324997477277913544")
 
 # def _updated_viewState(html):
 #     soup = BeautifulSoup(html)
@@ -253,3 +266,5 @@ print data
 #         print updates[1].text
 
 # _updated_viewState(html)
+
+
