2d44cdca2ca4d67a7d4cc2bf4d5fd526d6dd945c
Demian Florentin <demian@sinergetica.com>
2015-04-07 15:01:37 -0400

    code reordering and comments added

--- scrap.py
+++ scrap.py
@@ -90,116 +90,7 @@ class SilpyScrapper(object):
         conn.close()
         return response, data
 
-    def _extract_response_headers(self, headers):
-        """ recieves a tuple """
-        headers_dict = {}
-        set_cookie = 'Set-Cookie'
-        for header in headers:
-            if 'Date' in header:
-                #headers_dict['Date'] = header[header.index(":") : len(header)]
-                pass
-            else:
-                key, val = header.split(":")
-                headers_dict[key.strip()] = val.strip()
-        return headers_dict
-    
-    @DeprecationWarning
-    def _extract_parlamentary_ids(self, html):
-        """parlamentary id are contained on the images
-           in the form of images/<id>.jpg
-           returns a list of ids 
-        """
-    # TEST CODE #
-        html = ''
-        listarParlamentario = open('resources/lista_parlamentarios.html')
-        for l in listarParlamentario:
-            html +=l 
-    # END TEST CODE #
-        ids = []
-        soup = BeautifulSoup(html)
-        all_img = soup.find_all('img')
-        str_images = '/images'
-        
-        for img in all_img:
-            src = img['src']
-            if str_images in src:
-                ids.append(src[len(str_images)+1: len(src)].replace('.jpg',''))
-        return ids
-    def _download_attachment(self):
-        #TODO:
-        # content = 'formMain=formMain&formMain%3AdataTableDetalle%3A0%3Aj_idt178=&javax.faces.ViewState=-3644735490815418626%3A-2324997477277913544'
-        # return self._execute_REST_call('POST', '/formulario/VerDetalleTramitacion.pmf', content, headers, application_x_www_form_urlencoded)
-        pass
-        
-    def _extract_sesion_values(self, response, data):
-        self.headers = self._extract_response_headers(response.msg.headers)
-
-        #TODO: this section should go in a different method
-        # viewStates are the way the server knows what is being asked
-        # and what to respond, for different widgets there are different
-        # viewStates
-        soup = BeautifulSoup(data)
-        input = soup.find(id='javax.faces.ViewState')
-        self.viewState =  input['value']
-      
-    #@updateViewState
-    def _extract_parlamentary_data(self, html):
-        partial_soup = BeautifulSoup(html)
-        cdata_contents = []
-        #widgets updates come insade a CDATA
-        for cd in partial_soup.findAll(text=True):
-            if isinstance(cd, CData):
-                cdata_contents.append(cd)
-        table_section = cdata_contents[0]
-        viewState = cdata_contents[1]
-        
-        soup = BeautifulSoup(table_section)
-        tbody = soup.find(id="formMain:dataTable_data")
-        tr_list = tbody.find_all("tr")
-        rows = []
-        row = {}
-        str_images = '/images'
-        for tr in tr_list:
-            #this is a table, of course it has a fixed length
-            #0 = row_id, incremental
-            #1 = img (we extract the parlamentary id from the image name
-            #2 = name
-            #3 = committees: div with list of divs
-            #4 = projects: link using the extracted id from (2)
-
-            td_list = tr.find_all("td")
-            row['index'] = td_list[0].text.strip()
-            row['name'] = td_list[2].text.strip()
-            row['projects'] = td_list[4].text.strip()
-            row_index = int(row['index'])-1
-            #extraction of parlamentary id
-            src = td_list[1].div.img['src']
-            if str_images in src:
-                row['id'] = src[len(str_images)+1: len(src)].replace('.jpg','')
-
-                #extraction of formMain
-                lis = td_list[3].div.div.find_all("li")
-                committees = []
-                for li in lis:
-                    committee = {'text': li.text.strip(), 'formMain': li.a['id']}
-                    formMain = committee['formMain']+ '=' + committee['formMain']
-                    #viewState = self.viewState # "-3644735490815418626%3A-2324997477277913544"
-                    appended_string = 'javax.faces.ViewState='+ viewState + '&' + formMain
-                    #print (committee['text'], appended_string)
-                    committees.append(committee)
-                    row['committees'] = committees
-                    #download data                    
-                    body = committee_item_data + appended_string
-                    response, data = self.list_projects_by_committee(body)
-                    
-                    #
-                    #TODO: improve calls, handle response
-                    #
-                    print data
-                    #print row['index'] + " - " + row['name'] +" - " + row['id']
-                    #print "##################################################################"
-                    
-
+                           
     def list_projects_by_committee(self, data):
         headers = {}
         headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
@@ -282,6 +173,123 @@ class SilpyScrapper(object):
         stats = self._extract_statistics_table(html)
         projects = self._extract_projects_by_committee(html)
         return stats, projects
+
+
+##################################
+# HTML Parser Section
+#
+# As for now rest call code and html parsing ar too mixed, 
+# refactoring needed 
+##################################
+    def _extract_response_headers(self, headers):
+        """ recieves a tuple """
+        headers_dict = {}
+        set_cookie = 'Set-Cookie'
+        for header in headers:
+            if 'Date' in header:
+                #headers_dict['Date'] = header[header.index(":") : len(header)]
+                pass
+            else:
+                key, val = header.split(":")
+                headers_dict[key.strip()] = val.strip()
+        return headers_dict
+    
+    @DeprecationWarning
+    def _extract_parlamentary_ids(self, html):
+        """parlamentary id are contained on the images
+           in the form of images/<id>.jpg
+           returns a list of ids 
+        """
+    # TEST CODE #
+        html = ''
+        listarParlamentario = open('resources/lista_parlamentarios.html')
+        for l in listarParlamentario:
+            html +=l 
+    # END TEST CODE #
+        ids = []
+        soup = BeautifulSoup(html)
+        all_img = soup.find_all('img')
+        str_images = '/images'
+        
+        for img in all_img:
+            src = img['src']
+            if str_images in src:
+                ids.append(src[len(str_images)+1: len(src)].replace('.jpg',''))
+        return ids
+
+    def _download_attachment(self):
+        #TODO:
+        # content = 'formMain=formMain&formMain%3AdataTableDetalle%3A0%3Aj_idt178=&javax.faces.ViewState=-3644735490815418626%3A-2324997477277913544'
+        # return self._execute_REST_call('POST', '/formulario/VerDetalleTramitacion.pmf', content, headers, application_x_www_form_urlencoded)
+        pass
+
+    def _extract_sesion_values(self, response, data):
+        self.headers = self._extract_response_headers(response.msg.headers)
+
+        #TODO: this section should go in a different method
+        # viewStates are the way the server knows what is being asked
+        # and what to respond, for different widgets there are different
+        # viewStates
+        soup = BeautifulSoup(data)
+        input = soup.find(id='javax.faces.ViewState')
+        self.viewState =  input['value']
+      
+    #@updateViewState
+    def _extract_parlamentary_data(self, html):
+        partial_soup = BeautifulSoup(html)
+        cdata_contents = []
+        #widgets updates come insade a CDATA
+        for cd in partial_soup.findAll(text=True):
+            if isinstance(cd, CData):
+                cdata_contents.append(cd)
+        table_section = cdata_contents[0]
+        viewState = cdata_contents[1]
+        
+        soup = BeautifulSoup(table_section)
+        tbody = soup.find(id="formMain:dataTable_data")
+        tr_list = tbody.find_all("tr")
+        rows = []
+        row = {}
+        str_images = '/images'
+        for tr in tr_list:
+            #this is a table, of course it has a fixed length
+            #0 = row_id, incremental
+            #1 = img (we extract the parlamentary id from the image name
+            #2 = name
+            #3 = committees: div with list of divs
+            #4 = projects: link using the extracted id from (2)
+
+            td_list = tr.find_all("td")
+            row['index'] = td_list[0].text.strip()
+            row['name'] = td_list[2].text.strip()
+            row['projects'] = td_list[4].text.strip()
+            row_index = int(row['index'])-1
+            #extraction of parlamentary id
+            src = td_list[1].div.img['src']
+            if str_images in src:
+                row['id'] = src[len(str_images)+1: len(src)].replace('.jpg','')
+
+                #extraction of formMain
+                lis = td_list[3].div.div.find_all("li")
+                committees = []
+                for li in lis:
+                    committee = {'text': li.text.strip(), 'formMain': li.a['id']}
+                    formMain = committee['formMain']+ '=' + committee['formMain']
+                    #viewState = self.viewState # "-3644735490815418626%3A-2324997477277913544"
+                    appended_string = 'javax.faces.ViewState='+ viewState + '&' + formMain
+                    #print (committee['text'], appended_string)
+                    committees.append(committee)
+                    row['committees'] = committees
+                    #download data                    
+                    body = committee_item_data + appended_string
+                    response, data = self.list_projects_by_committee(body)
+                    
+                    #
+                    #TODO: improve calls, handle response
+                    #
+                    print data
+                    #print row['index'] + " - " + row['name'] +" - " + row['id']
+                    #print "##################################################################"
 
     def _extract_statistics_table(self, html):
         #extracts data from the statistics table in resources/projects_by_committee.html
