2a84253c9122452c350b04407bbf1c5a143792b2
Demian Florentin <demian@sinergetica.com>
2015-04-20 22:03:36 -0400

    save comisiones por periodo

--- mongo_db.py
+++ mongo_db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 
-class SilpyClient(object):
+class SilpyMongoClient(object):
     
 
     def __init__(self):
@@ -15,7 +15,16 @@ class SilpyClient(object):
         return results
 
 
+    def save_senadores(self, senadores):
+        db_senadores = self.db.senadores #initialize collection
+        results = db_senadores.insert_many(senadores)# insert projects
+        return results
 
+    def save_comisiones_por_periodo(self, periodo, comisiones):
+        db_comisiones_periodo = self.db.comisiones_periodo
+        
+        comisiones_periodo = {'periodo': periodo, 
+                              'comisiones': comisiones}
 
-
-
+        result_ids = db_comisiones_periodo.insert_one(comisiones_periodo) 
+        return result_ids
