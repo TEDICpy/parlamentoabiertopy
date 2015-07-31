01e9b5ac6e41999b80266d793afb64b579c259f7
Ivan Florentin <ivan@sinergetica.com>
2015-07-31 16:54:23 -0400

    current port

--- docker/legislative/init.sh
+++ docker/legislative/init.sh
@@ -1,4 +1,7 @@
 #echo $POPIT_PORT_3000_TCP_ADDR " popit.parlamentoabierto.org.py popit" >> /etc/hosts
 #echo $POPIT_PORT_3000_TCP_ADDR " parlamento.popit.parlamentoabierto.org.py parlamento" >> /etc/hosts
 
-cd $legislative && rails server --port 8003
+#cd $legislative && rails server --port 5000
+
+cd $legislative && rails server --port 5000
+
