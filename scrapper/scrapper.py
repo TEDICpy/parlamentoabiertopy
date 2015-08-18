1e043c115bcb0d8161861bcc41001279091ffce9
demian <demian@sinergetica.com>
2015-08-18 19:58:46 -0400

    changed scrapping order

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -13,19 +13,17 @@ def cli():
 @click.command(help='extrae datos de senadores y diputados')
 def all():
     print 'corriendo todos los scrappers'
+    #senadores 
     silpy_senadores = SilpyScrapper()
-    silpy_diputados = SilpyScrapper()
-
     silpy_senadores.get_members_data('S')
-    silpy_senadores.close_navigator()
-
-    silpy_diputados.get_members_data('D')
-    silpy_diputados.close_navigator()
-
+    silpy_senadores.close_navigator()    
     senadores_scrapper = SenadoresScrapper()
     senadores_scrapper.get_all_articles()
     senadores_scrapper.extract_senators_data()
-
+    #diputados
+    silpy_diputados = SilpyScrapper()    
+    silpy_diputados.get_members_data('D')
+    silpy_diputados.close_navigator()
     diputados_scrapper = DiputadosScrapper()
     diputados_scrapper.get_members_data()
 
