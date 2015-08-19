b633525a8f126a8a8e134f81031b37369d7540a4
demian <demian@sinergetica.com>
2015-08-19 18:12:46 -0400

    store cv as html, --no-silpy scrapping option added

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -28,21 +28,27 @@ def all():
     diputados_scrapper.get_members_data()
 
 @click.command(help='Extrae solo los datos de los senadores.')
-def senadores():
+@click.option('--no-silpy', is_flag=True, help="No descarga datos del silpy.")
+def senadores(no_silpy=False):
     print 'scrapping senadores'
-    silpy_senadores = SilpyScrapper()
-    silpy_senadores.get_members_data('S')
-    silpy_senadores.close_navigator()
+    if not no_silpy:
+        silpy_senadores = SilpyScrapper()
+        silpy_senadores.get_members_data('S')
+        silpy_senadores.close_navigator()
+
     senadores_scrapper = SenadoresScrapper()
     senadores_scrapper.extract_senators_data()
     #senadores_scrapper.get_all_articles()
 
 @click.command(help="Extrae solo los datos de los diputados.")
+@click.option('--no-silpy', is_flag=True, help="No descarga datos del silpy.")
 def diputados():
     print 'scrapping diputados'
-    silpy_diputados = SilpyScrapper()
-    silpy_diputados.get_members_data('D')
-    silpy_diputados.close_navigator()
+    if not no_silpy:
+        silpy_diputados = SilpyScrapper()
+        silpy_diputados.get_members_data('D')
+        silpy_diputados.close_navigator()
+
     diputados_scrapper = DiputadosScrapper()
     diputados_scrapper.get_members_data()
 
@@ -55,8 +61,7 @@ def map_popit_data():
 @click.option('--origin', default='all', help="Origen de datos, 'all' para todos," +
               " 's' para senadores y 'd' para diputados.")
 @click.option('--new', is_flag=True, help="Descarga solo nuevos projectos de ley. Disponible solo para 'all'.")
-def bills(origin, new=False):
-    
+def bills(origin, new=False):    
     if origin == 'all':
         silpy_scrapper = SilpyScrapper()
         silpy_scrapper.download_all_bills(new)
