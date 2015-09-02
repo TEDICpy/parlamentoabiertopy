f5369541d4be2f342d533d36b2b6ca44043128f4
Ivan Florentin <ivan@sinergetica.com>
2015-09-01 20:33:49 -0400

    writeit update

--- docker/init/writeit-init.sh
+++ docker/init/writeit-init.sh
@@ -3,6 +3,7 @@
 cd $writeit
 pip install -r requirements.txt
 #ython manage.py createsuperuse
+service elasticsearch start
 python manage.py runserver 0.0.0.0:8001
 
 
--- docker/writeit/build_image.sh
+++ docker/writeit/build_image.sh
@@ -1,11 +1,11 @@
 docker build -t writeit_temp .
-echo "Ingresar los siguientes comando en consola: "
-echo "    $ cd /src/write-it && ./manage.py syncdb "
-echo "si va un error, volver a correr el comando "
-echo "Luego correr el siguiente comando: "
-echo " cd /src/write-it && ./manage.py migrate"
-echo "Si no hay errores, salir del contenedor con control+d o $ exit  "
-docker run --name writeit_temp  -ti --volumes-from="data" --net="host" -v /src writeit_temp /bin/bash
-docker commit -a "Ivan Florentin" -m "writeit dbconfig" -p writeit_temp  writeit
-docker create --name writeit  --volumes-from="data" --net="host" -v /src writeit  python $writeit/manage.py runserver 0.0.0.0:8000
+#echo "Ingresar los siguientes comando en consola: "
+#echo "    $ cd /src/write-it && ./manage.py syncdb "
+#echo "si va un error, volver a correr el comando "
+#echo "Luego correr el siguiente comando: "
+#echo " cd /src/write-it && ./manage.py migrate"
+#echo "Si no hay errores, salir del contenedor con control+d o $ exit  "
+#docker run --name writeit_temp  -ti --volumes-from="data" --net="host" -v /src writeit_temp /bin/bash
+#docker commit -a "Ivan Florentin" -m "writeit dbconfig" -p writeit_temp  writeit
+#docker create --name writeit  --volumes-from="data" --net="host" -v /src writeit  python $writeit/manage.py runserver 0.0.0.0:8000
 
