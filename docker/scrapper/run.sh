3af0d8801196138fe16f3f3e7880cccb4e4948f1
Lu Pa <lupa@inventati.org>
2016-05-16 15:19:31 -0400

    el archivo que realmente esta en prod

--- docker/scrapper/run.sh
+++ docker/scrapper/run.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-docker run --net="host" -ti --rm --name scrapper -v $PA_SRC:/src scrapper /bin/bash
+ docker run --net="host" --rm --name scrapper -ti -v $PA_SRC:/src --volumes-from data scrapper /bin/bash
